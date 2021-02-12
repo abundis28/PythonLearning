674c410040776e35ec8d59b5f316f3909d3b8121
Andrés Abundis <andresabundis@gmail.com>
2021-02-11 22:12:33 -0600

    Adds menu for user to select which option to run.

---
+++ Basics.py
@@ -0,0 +1,15 @@
+### MAIN ###
+print("Welcome to the Python basics demonstration!!!")
+print()
+option = int(input("Please select an option of demonstration to run: "))
+while option != 4:
+    if option == 1:
+        print("Run first option")
+    elif option == 2:
+        print("Run second option")
+    elif option == 3:
+        print("Run third option")
+    print()
+    option = int(input("Please select another option of demonstration to run: "))
+
+print("Thanks for running this code!")
